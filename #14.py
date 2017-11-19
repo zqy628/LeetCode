ec5571824613147b4bcea352a004f0b76e7b1900
zqy628 <191155395@qq.com>
2017-11-19 17:42:24 +0800

    add #15

--- #14.py
+++ #14.py
@@ -9,3 +9,25 @@ class Solution:
         :type strs: List[str]
         :rtype: str
         """
+        prefix = ''
+        lenth = len(strs)
+        if not strs:
+            return ''
+        else:
+            i = 1
+            while i <= len(strs[0]):
+                try:
+                    prefix_ = strs[0][:i]
+                except:
+                    return prefix
+                else:
+                    for j in range(1,lenth):
+                        if prefix_ == strs[j][:i]:
+                            continue
+                        else:
+                            return prefix
+                prefix = prefix_
+                i += 1
+            return prefix
+s = Solution()
+print(s.longestCommonPrefix(['']))
