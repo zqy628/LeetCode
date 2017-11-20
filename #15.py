1e4e6e1ede8745f139a4614608d7541c44739ee5
zqy628 <191155395@qq.com>
2017-11-20 18:45:14 +0800

    add #15

--- #15.py
+++ #15.py
@@ -13,10 +13,25 @@ A solution set is:
   [-1, -1, 2]
 ]
 '''
+import itertools
 class Solution:
     def threeSum(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
+        l = []
+        r = [] #存放重复元素
+        #找出重复元素
+        for i,k in itertools.groupby(nums,len):
+            if i >= 2:
+                r += k
+        nums = list(set(nums)) #去重
+        #创建一个迭代器，返回iterable中所有长度为r的子序列,不带重复
+        for i in itertools.combinations(nums,3):
+            if sum(i) == 0:
+                l.append(list(i))
+        if r:
+            for j in r:
+                
+        return l
