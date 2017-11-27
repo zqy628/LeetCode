b4faedbd647614ded9d82ddacba3564bc1e16f16
zqy628 <191155395@qq.com>
2017-11-27 13:38:10 +0800

    add #23

---
+++ #23.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+'''
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        
